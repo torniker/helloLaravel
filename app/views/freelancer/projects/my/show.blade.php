fb5f1f51ed59a7c88b1c7e357e0b0abd34011de6
nch7 <nikabika97@gmail.com>
2015-02-13 13:29:22 +0400

    fix: project details page

--- app/views/freelancer/projects/my/show.blade.php
+++ app/views/freelancer/projects/my/show.blade.php
@@ -14,7 +14,6 @@
 		<div class='margin-top-big'>
 			<h4>Offers</h4>
 			@foreach($project->offers as $offer)
-<<<<<<< HEAD
 			<div class='ibox border-bottom' style="margin-bottom:0">
 				<div class='ibox-title ibox-title-padding collapse-link icon clearfix'>
 					<div class="pull-left" >
@@ -22,26 +21,13 @@
 						<b class="label label-primary floatdel"  > {{ $offer->price }} {{ $offer->currencyText() }}</b>
 					</div>
 					<div class="ibox-tools">
-                        <a class="collapse-link">
+                        <a class="">
                             <i class="fa fa-chevron-up"></i>
                         </a>
 					</div>
 				</div>
 				<div class='ibox-content custom-padding hide-by-default'> 
 					<p class='margin-sm-top' style="margin:0">{{ $offer->message }} {{ $offer->message }} {{ $offer->message }} {{ $offer->message }}</p>
-=======
-				<div class="panel panel-default offer">
-				  <div class="panel-body">
-				    @if($offer->isHired())
-				    	hired! 
-				    @else
-				    	<a clas='btn btn-xs btn-success' href='{{ URL::to("freelancer/projects/my/".$project->id."/hire",$offer->id) }}'>Hire</a>
-				    @endif
-				    <div>{{ $offer->user->firstname }} {{ $offer->user->lastname }}</div>
-				    <div>Offer Price: {{ $offer->price }} {{ $offer->currencyText() }}</div>
-				    <p class='one-line-text message margin-sm-top'>{{ $offer->message }} {{ $offer->message }} {{ $offer->message }} {{ $offer->message }}</p>
-				  </div>
->>>>>>> 9372c54ee688216ab44fc6e4eb76058e7f5dcc72
 				</div>
 			</div>
 			@endforeach
